04877e1bda6ea99172cf4402e190b7f0254e3d4d
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-05-25 14:41:50 +0530

    changed attribute names in the Settings class from lowercase to uppercase

--- app/config.py
+++ app/config.py
@@ -1,13 +1,13 @@
 import os
 
 class Settings:
-    base_dir = os.path.dirname(os.path.abspath(__file__))
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     
-    category_classifier_model_path: str = os.path.join(base_dir, "ml/models/category_classifier.joblib")
-    category_classifier_vectorizer_path: str = os.path.join(base_dir, "ml/vectorizers/category_classifier/vectorizer.joblib")
-    priority_classifier_model_path: str = os.path.join(base_dir, "ml/models/priority_classifier.joblib")
-    priority_classifier_vectorizer_path: str = os.path.join(base_dir, "ml/vectorizers/priority_classifier/vectorizer.joblib")
-    category_encoder_path: str = os.path.join(base_dir, "ml/encoders/category_encoder.joblib")
+    CATEGORY_CLASSIFIER_MODEL_PATH: str = os.path.join(BASE_DIR, "ml/models/category_classifier.joblib")
+    CATEGORY_CLASSIFIER_VECTORIZER_PATH: str = os.path.join(BASE_DIR, "ml/vectorizers/category_classifier/vectorizer.joblib")
+    PRIORITY_CLASSIFIER_MODEL_PATH: str = os.path.join(BASE_DIR, "ml/models/priority_classifier.joblib")
+    PRIORITY_CLASSIFIER_VECTORIZER_PATH: str = os.path.join(BASE_DIR, "ml/vectorizers/priority_classifier/vectorizer.joblib")
+    CATEGORY_ENCODER_PATH: str = os.path.join(BASE_DIR, "ml/encoders/category_encoder.joblib")
 
 CATEGORY_LABELS = [
     "Technical Support",
