3efe0b81fd69d216f4779703a992ebd6c0eef804
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-05-25 19:29:51 +0530

    updated model paths

--- app/config.py
+++ app/config.py
@@ -3,9 +3,9 @@ import os
 class Settings:
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     
-    CATEGORY_CLASSIFIER_MODEL_PATH: str = os.path.join(BASE_DIR, "ml/models/category_classifier.joblib")
+    CATEGORY_CLASSIFIER_MODEL_PATH: str = os.path.join(BASE_DIR, "ml/models/category_classifier.gzip")
     CATEGORY_CLASSIFIER_VECTORIZER_PATH: str = os.path.join(BASE_DIR, "ml/vectorizers/category_classifier/vectorizer.joblib")
-    PRIORITY_CLASSIFIER_MODEL_PATH: str = os.path.join(BASE_DIR, "ml/models/priority_classifier.joblib")
+    PRIORITY_CLASSIFIER_MODEL_PATH: str = os.path.join(BASE_DIR, "ml/models/priority_classifier.gzip")
     PRIORITY_CLASSIFIER_VECTORIZER_PATH: str = os.path.join(BASE_DIR, "ml/vectorizers/priority_classifier/vectorizer.joblib")
     CATEGORY_ENCODER_PATH: str = os.path.join(BASE_DIR, "ml/encoders/category_encoder.joblib")
 
