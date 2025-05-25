b39fa1140ede456cf0976d0c03509209b83ddaaf
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-05-25 14:12:14 +0530

    added category and priority labels list to config

--- app/config.py
+++ app/config.py
@@ -8,3 +8,22 @@ class Settings:
     priority_classifier_model_path: str = os.path.join(base_dir, "ml/models/priority_classifier.joblib")
     priority_classifier_vectorizer_path: str = os.path.join(base_dir, "ml/vectorizers/priority_classifier/vectorizer.joblib")
     category_encoder_path: str = os.path.join(base_dir, "ml/encoders/category_encoder.joblib")
+
+CATEGORY_LABELS = [
+    "Technical Support",
+    "Product Support",
+    "Customer Service",
+    "IT Support",
+    "Billing and Payments",
+    "Returns and Exchanges",
+    "Service Outages and Maintenance",
+    "Sales and Pre-Sales",
+    "Human Resources",
+    "General Inquiry",
+]
+
+PRIORITY_LABELS = [
+    "Low",
+    "Medium",
+    "High",
+]
